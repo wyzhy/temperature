f6628a510f42f0cdde5923d2a7bba7af8f9b0100
pqx <3274725296@qq.com>
2021-12-13 14:59:08 +0800

    1：spring官网初始化项目，提交项目代码。

---
+++ src/main/java/com/wy/pqx/temperature/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wy.pqx.temperature;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TemperatureApplication.class);
+	}
+
+}
---
+++ src/main/java/com/wy/pqx/temperature/TemperatureApplication.java
@@ -0,0 +1,13 @@
+package com.wy.pqx.temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureApplication.class, args);
+	}
+
+}
